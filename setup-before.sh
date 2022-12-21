a940dda9247090922d92b8368bbc989a0787ac02
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-12-21 10:17:18 +0000

    update

--- setup-before.sh
+++ setup-before.sh
@@ -1,3 +1,3 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y build-essential wget curl binutils make perl libx11-6 libx11-dev xliblg xliblg-dev tcsh
+sudo apt install -y build-essential wget curl binutils make perl libx11-6 libx11-dev zliblg zliblg-dev tcsh
