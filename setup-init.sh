5f40c21ef46ce850f59e4bcffc1f97a3348b6df2
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-12-21 11:43:04 +0000

    update

--- setup-init.sh
+++ setup-init.sh
@@ -6,7 +6,8 @@ tar xvfz software/mesasdk-x86_64-linux-22.6.1.tar.gz -C software/
 echo "export MESASDK_ROOT=/workspace/MESA/software/mesasdk" >> ~/.bash_profile
 echo "source $MESASDK_ROOT/bin/mesasdk_init.sh" >> ~/.bash_profile
 
-curl https://downloads.sourceforge.net/project/mesa/releases/mesa-r15140.zip --output software/mesa-r15140.zip
+curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output software/mesa-r15140.zip
+unzip software/mesa-r15140.zip
 
 echo "export MESA_DIR=/workspace/MESA/software/mesa-r15140" >> ~/.bash_profile
 echo "export OMP_NUM_THREADS=2" >> ~/.bash_profile
