ceb9db08e0918c73f23b994ed44124ecaead001e
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-12-21 19:22:12 +0000

    update

--- install-mesa.sh
+++ install-mesa.sh
@@ -5,7 +5,7 @@ tar xvfz ~/software/mesasdk-x86_64-linux-22.6.1.tar.gz -C ~/software/
 rm -rf ~/software/mesasdk-x86_64-linux-22.6.1.tar.gz
 
 echo "export MESASDK_ROOT=~/software/mesasdk" >> ~/.bash_profile
-echo "source ~/software/bin/mesasdk_init.sh" >> ~/.bash_profile
+echo "source ~/software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
 
 curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output ~/software/mesa-r15140.zip
 unzip ~/software/mesa-r15140.zip -d ~/software/
@@ -14,7 +14,7 @@ rm -rf ~/software/mesa-r15140.zip
 echo "export MESA_DIR=~/software/mesa-r15140" >> ~/.bash_profile
 echo "export OMP_NUM_THREADS=2" >> ~/.bash_profile
 
-echo "export MESA_DIR=~/software/mesa-r15140" >> ~/.bashrc
+echo "source ~/.bash_profile" >> ~/.bashrc
 source ~/.bash_profile
 
 cd ~/software/mesa-r15140
