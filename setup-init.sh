ed73efc18cd11706458c1d508ffde35e34e5ba66
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2022-12-21 21:47:03 +0530

    Update setup-init.sh

--- setup-init.sh
+++ setup-init.sh
@@ -1,15 +1,15 @@
 mkdir software
 
-curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-22.6.1.tar.gz --output software/mesasdk-x86_64-linux-22.6.1.tar.gz
-tar xvfz software/mesasdk-x86_64-linux-22.6.1.tar.gz -C software/
+curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-22.6.1.tar.gz --output ~/software/mesasdk-x86_64-linux-22.6.1.tar.gz
+tar xvfz ~/software/mesasdk-x86_64-linux-22.6.1.tar.gz -C ~/software/
 
 echo "export MESASDK_ROOT=/workspace/MESA/software/mesasdk" >> ~/.bash_profile
 echo "source $MESASDK_ROOT/bin/mesasdk_init.sh" >> ~/.bash_profile
 
 curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output software/mesa-r15140.zip
-unzip software/mesa-r15140.zip
+unzip ~/software/mesa-r15140.zip -d ~/software/
 
-echo "export MESA_DIR=/workspace/MESA/software/mesa-r15140" >> ~/.bash_profile
+echo "export MESA_DIR=~/software/mesa-r15140" >> ~/.bash_profile
 echo "export OMP_NUM_THREADS=2" >> ~/.bash_profile
 
 cd $MESA_DIR
