b3fb7ddd21efc68a35258aaaa34706398a769695
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-12-22 10:09:25 +0000

    update

--- install-mesa.sh
+++ install-mesa.sh
@@ -1,18 +1,17 @@
 sudo apt update
-sudo apt -y upgrade
 sudo apt install -y build-essential wget curl binutils make perl libx11-6 libx11-dev zlib1g zlib1g-dev tcsh
 
-mkdir /workspaces/software
+mkdir software
 
-curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz
-tar xvfz /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz -C /workspaces/software/
-rm -rf /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz
+curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output software/mesasdk-x86_64-linux-21.4.1.tar.gz
+tar xvfz software/mesasdk-x86_64-linux-21.4.1.tar.gz -C software/
+rm -rf software/mesasdk-x86_64-linux-21.4.1.tar.gz
 
-echo "export MESASDK_ROOT=/workspaces/software/mesasdk" >> ~/.bash_profile
-echo "source ~/software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
+echo "export MESASDK_ROOT=software/mesasdk" >> ~/.bash_profile
+echo "source software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
 
-curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output /workspaces/software/mesa-r15140.zip
-unzip /workspaces/software/mesa-r15140.zip -d /workspaces/software/
+curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output software/mesa-r15140.zip
+unzip software/mesa-r15140.zip -d /workspaces/software/
 rm -rf /workspaces/software/mesa-r15140.zip
 
 echo "export MESA_DIR=/workspaces/software/mesa-r15140" >> ~/.bash_profile
