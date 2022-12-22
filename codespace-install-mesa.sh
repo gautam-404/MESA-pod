d2a26d6f8c6163e65b679b855a4b2e0ca9377e57
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2022-12-22 19:53:07 +0000

    update

--- codespace-install-mesa.sh
+++ codespace-install-mesa.sh
@@ -6,6 +6,11 @@ yes | sudo apt install build-essential wget curl binutils make perl libx11-6 lib
 
 ## Install MESA
 DIR="/workspaces/software"
+echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
+echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
+echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
+echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
+
 if [ ! -d "$DIR" ]
 then
     mkdir $DIR
@@ -17,20 +22,16 @@ then
     curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output $DIR/mesa-r15140.zip
     unzip $DIR/mesa-r15140.zip -d $DIR/
     rm -rf $DIR/mesa-r15140.zip
+    source ~/.zshrc
+    cd $DIR/mesa-r15140
+    ./install
 else
+    source ~/.zshrc
     cd $MESA_DIR
+    cd $DIR/mesa-r15140
     ./clean
     ./install
 fi
-
-echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
-echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
-echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
-echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
-source ~/.zshrc
-
-cd $DIR/mesa-r15140
-./install
 
 ## Install GYRE
 export GYRE_DIR=$MESA_DIR/gyre/gyre
