d58e5dec0e1d88e8255c4e569103c806d441645f
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2022-12-22 13:56:17 +0000

    update

--- codespace-install-mesa.sh
+++ codespace-install-mesa.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y build-essential wget curl binutils make perl libx11-6 libx11-dev zlib1g zlib1g-dev tcsh
+yes | sudo apt install build-essential wget curl binutils make perl libx11-6 libx11-dev zlib1g zlib1g-dev tcsh
 
 mkdir /workspaces/software
 
