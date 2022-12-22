7d02d38a3bed786e176732d05f33a2cfd37bbbf4
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2022-12-22 19:29:36 +0000

    update

--- codespace-install-mesa.sh
+++ codespace-install-mesa.sh
@@ -7,26 +7,27 @@ chsh -s $(which zsh)
 yes | sudo apt install build-essential wget curl binutils make perl libx11-6 libx11-dev zlib1g zlib1g-dev tcsh
 
 ## Install MESA
-mkdir /workspaces/software
+DIR="/workspaces/software"
+if [ ! -d "$DIR" ]
+then
+    mkdir $DIR
+    # sdk
+    curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
+    tar xvfz $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz -C $DIR/
+    rm -rf $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
+    #mesa
+    curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output $DIR/mesa-r15140.zip
+    unzip $DIR/mesa-r15140.zip -d $DIR/
+    rm -rf $DIR/mesa-r15140.zip
+fi
 
-curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz
-tar xvfz /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz -C /workspaces/software/
-rm -rf /workspaces/software/mesasdk-x86_64-linux-21.4.1.tar.gz
+echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
+echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
+echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
+echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
+source ~/.zshrc
 
-echo "export MESASDK_ROOT=/workspaces/software/mesasdk" >> ~/.bash_profile
-echo "source /workspaces/software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
-
-curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output /workspaces/software/mesa-r15140.zip
-unzip /workspaces/software/mesa-r15140.zip -d /workspaces/software/
-rm -rf /workspaces/software/mesa-r15140.zip
-
-echo "export MESA_DIR=/workspaces/software/mesa-r15140" >> ~/.bash_profile
-echo "export OMP_NUM_THREADS=2" >> ~/.bash_profile
-
-source ~/.bash_profile
-echo "source ~/.bash_profile" >> ~/.bashrc
-
-cd /workspaces/software/mesa-r15140
+cd $DIR/mesa-r15140
 ./install
 
 ## Install GYRE
