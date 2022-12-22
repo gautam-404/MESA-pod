9d16426b6b05e4583df2f962726f3bd52a073221
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2022-12-22 19:41:08 +0000

    update

--- codespace-install-mesa.sh
+++ codespace-install-mesa.sh
@@ -25,7 +25,6 @@ echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
 echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
 echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
 echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
-exec zsh
 source ~/.zshrc
 
 cd $DIR/mesa-r15140
