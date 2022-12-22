22186aa29faba314f39698bf2a0a605cd2eb55e7
Anuj Gautam <gautam.anuj.1195@gmail.com>
2022-12-22 11:03:08 +0000

    update

--- install-mesa.sh
+++ install-mesa.sh
@@ -1,21 +1,21 @@
-mkdir software
+mkdir /workspace/MESA/software
 
-curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output software/mesasdk-x86_64-linux-21.4.1.tar.gz
-tar xvfz software/mesasdk-x86_64-linux-21.4.1.tar.gz -C software/
-rm -rf software/mesasdk-x86_64-linux-21.4.1.tar.gz
+curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output /workspace/MESA/software/mesasdk-x86_64-linux-21.4.1.tar.gz
+tar xvfz /workspace/MESA/software/mesasdk-x86_64-linux-21.4.1.tar.gz -C /workspace/MESA/software/
+rm -rf /workspace/MESA/software/mesasdk-x86_64-linux-21.4.1.tar.gz
 
-echo "export MESASDK_ROOT=software/mesasdk" >> ~/.bash_profile
-echo "source software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
+echo "export MESASDK_ROOT=/workspace/MESA/software/mesasdk" >> ~/.bash_profile
+echo "source /workspace/MESA/software/mesasdk/bin/mesasdk_init.sh" >> ~/.bash_profile
 
-curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output software/mesa-r15140.zip
-unzip software/mesa-r15140.zip -d software/
-rm -rf software/mesa-r15140.zip
+curl https://zenodo.org/record/4311514/files/mesa-r15140.zip?download=1 --output /workspace/MESA/software/mesa-r15140.zip
+unzip /workspace/MESA/software/mesa-r15140.zip -d /workspace/MESA/software/
+rm -rf /workspace/MESA/software/mesa-r15140.zip
 
-echo "export MESA_DIR=software/mesa-r15140" >> ~/.bash_profile
+echo "export MESA_DIR=/workspace/MESA/software/mesa-r15140" >> ~/.bash_profile
 echo "export OMP_NUM_THREADS=2" >> ~/.bash_profile
 
 source ~/.bash_profile
 echo "source ~/.bash_profile" >> ~/.bashrc
 
-cd $MESA_DIR
+cd /workspace/MESA/software/mesa-r15140
 ./install
