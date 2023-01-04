ea972bd9ddfb5b6731408638ba406f4fdd4fe8e4
Anuj Gautam <gautam.anuj.1195@gmail.com>
2023-01-04 11:32:42 +0000

    gitpod restart issues solved

--- gitpod-install-mesa.sh
+++ gitpod-install-mesa.sh
@@ -1,31 +1,63 @@
+#! /bin/zsh
+
 ## Install MESA
 DIR="/workspace/software"
-mkdir $DIR
-# sdk
-curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
-tar xvfz $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz -C $DIR/
-rm -rf $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
-#mesa
-curl https://zenodo.org/record/4311514/files/mesa-r15140.zip --output $DIR/mesa-r15140.zip
-unzip $DIR/mesa-r15140.zip -d $DIR/
-rm -rf $DIR/mesa-r15140.zip
 
-echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
-echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
-echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
-echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
+install_mesa()
+{
+    mkdir $DIR
+    # sdk
+    curl http://user.astro.wisc.edu/~townsend/resource/download/mesasdk/mesasdk-x86_64-linux-21.4.1.tar.gz --output $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
+    tar xvfz $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz -C $DIR/
+    rm -rf $DIR/mesasdk-x86_64-linux-21.4.1.tar.gz
+    #mesa
+    curl https://zenodo.org/record/4311514/files/mesa-r15140.zip --output $DIR/mesa-r15140.zip
+    unzip $DIR/mesa-r15140.zip -d $DIR/
+    rm -rf $DIR/mesa-r15140.zip
 
-source ~/.zshrc
+    echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
+    echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
+    echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
+    echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
+
+    source ~/.zshrc
 
 
-cd $DIR/mesa-r15140
-./install
+    cd $DIR/mesa-r15140
+    ./install
 
-## Install GYRE
-export GYRE_DIR=$MESA_DIR/gyre/gyre
-cd $GYRE_DIR
-make
+    ## Install GYRE
+    export GYRE_DIR=$MESA_DIR/gyre/gyre
+    cd $GYRE_DIR
+    make
 
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-wget https://gist.githubusercontent.com/renbaoshuo/93f75878fe29cf9ad48a675abd69ef97/raw/3b4ddd4f190af6c6466e07da0850618a9c208353/codespaces.zsh-theme -O ~/.oh-my-zsh/custom/themes/codespaces.zsh-theme
-omz theme set codespaces
+    echo "Done!"
+}
+
+if [ -d "$DIR/mesasdk" ] && [ -d "$DIR/mesa-r15140" ];
+then
+    echo "MESA already exists!!"
+    echo "Set environment variables again and re-write them to .*rc/.*profile?"
+    read response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo "export MESASDK_ROOT=$DIR/mesasdk" >> ~/.zshrc
+        echo "source $DIR/mesasdk/bin/mesasdk_init.sh" >> ~/.zshrc
+        echo "export MESA_DIR=$DIR/mesa-r15140" >> ~/.zshrc
+        echo "export OMP_NUM_THREADS=2" >> ~/.zshrc
+        source ~/.zshrc
+        echo "Done!"
+        echo "Please run: source ~/.zshrc"
+    else  
+        echo "Remove and perform a clean re-install?"
+        read response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            install_mesa
+        else
+            echo "Skipping!"
+        fi
+    fi
+else
+    install_mesa
+fi  
