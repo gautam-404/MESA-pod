28ff437c99362c6baa531b229ee4801a54b9162f
Anuj Gautam <gautam.anuj.1195@gmail.com>
2023-01-11 11:31:09 +0000

    new

--- script.py
+++ script.py
@@ -1,26 +1,24 @@
 from MesaHandler.MesaProjHandler import ProjectOps
 from MesaHandler import MesaAccess
-# from alive_progress import alive_bar
+import glob
+from alive_progress import alive_bar
+
+inlists = glob.glob("inlists/*_inlist_*")
+print(inlists)
 
 dsct = ProjectOps("dsct")       
 dsct.create(overwrite=True, clean=True)             
 dsct.make()
-dsct.loadPGstarInlist( "inlists/pgstar_dsct" )
-
-dsct.loadProjInlist( "inlists/1_test" )
-# dsct.run(silent=True)
-dsct.run()
-
-dsct.loadProjInlist( "inlists/6_test" )
-# dsct.run(silent=True)
-dsct.run()
 
 
-# with alive_bar(unknown="waves") as bar:
-#     for i in range(1,3):
-#         dsct.loadProjInlist( "inlists/%i_test"%(i+1) )
-#         dsct.run(silent=True)
-#         bar()
+with alive_bar(unknown="waves") as bar:
+    for i in range(1,3):
+        dsct.loadProjInlist(inlists[i])
+        dsct.run(silent=True)
+        file = open("runlog", "a")  # append mode
+        file.write( "\n\n"+("*"*100)+"\n\n" )
+        file.close()
+        bar()
 
 
 # dsct.rerun("x450")
