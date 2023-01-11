1240454f316437da60002891e5bb0475b4b20c9d
Anuj Gautam <gautam.anuj.1195@gmail.com>
2023-01-11 11:32:55 +0000

    new

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ import glob
 from alive_progress import alive_bar
 
 inlists = glob.glob("inlists/*_inlist_*")
-print(inlists)
 
 dsct = ProjectOps("dsct")       
 dsct.create(overwrite=True, clean=True)             
@@ -15,7 +14,7 @@ with alive_bar(unknown="waves") as bar:
     for i in range(1,3):
         dsct.loadProjInlist(inlists[i])
         dsct.run(silent=True)
-        file = open("runlog", "a")  # append mode
+        file = open("dsct/runlog", "a")  # append mode
         file.write( "\n\n"+("*"*100)+"\n\n" )
         file.close()
         bar()
