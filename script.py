1df79c5928e437afc150be786600c635d3a4e4a1
Anuj Gautam <gautam.anuj.1195@gmail.com>
2023-01-11 11:43:18 +0000

    update

--- script.py
+++ script.py
@@ -9,12 +9,11 @@ dsct = ProjectOps("dsct")
 dsct.create(overwrite=True, clean=True)             
 dsct.make()
 
-
 with alive_bar(unknown="waves") as bar:
-    for i in range(1,3):
+    for i in range(6):
         dsct.loadProjInlist(inlists[i])
         dsct.run(silent=True)
-        file = open("dsct/runlog", "a")  # append mode
+        file = open("/workspace/MESA/dsct/runlog", "a")  # append mode
         file.write( "\n\n"+("*"*100)+"\n\n" )
         file.close()
         bar()
