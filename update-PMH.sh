7a3996a04caf12f9e56689ef372fd94267269676
Anuj Gautam <45739450+gautam-404@users.noreply.github.com>
2023-01-14 15:22:42 +0530

    Update update-PMH.sh

--- update-PMH.sh
+++ update-PMH.sh
@@ -1,8 +1,13 @@
-#! /usr/bin/zsh
+#! /usr/bin/env zsh
 
-cd $HOME/software
+if [[ "$OSTYPE" == "linux"* ]]; then
+    cd $HOME/software     ## For my gitpod
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    cd /Users/anuj/Documents/MESA-workspace     ## for my mac
+fi
+
 rm -rf PyMesaHandler
 git clone --recurse-submodules https://github.com/gautam-404/PyMesaHandler.git
 cd PyMesaHandler
-pip install -r requirements.txt
-pip install .
+pip3 install -r requirements.txt
+pip3 install .
